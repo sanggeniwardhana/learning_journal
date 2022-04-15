4b468867d2ab49365f48958486bed21daabe0f2c
Sanggeni Gali Wardhana <sanggeniwardhana@gmail.com>
2022-04-16 01:59:27 +0700

    Recap Week 1

---
+++ phase_0/week_1/main_d3am.py
@@ -0,0 +1,4 @@
+from package_d3am.modul_d3am import *
+
+print(kecepatan(10,5))
+print(volume(2,4,6))
---
+++ phase_0/week_1/package_d3am/modul_d3am.py
@@ -0,0 +1,14 @@
+def tambah (x,y):
+    return x+y
+def perkalian(x,y):
+    return x*y
+def volume(panjang,lebar,tinggi):
+    '''
+    ini adalah rumus volume balok
+    '''
+    return panjang*lebar*tinggi
+def kecepatan (jarak,waktu):
+    '''
+    ini adalah rumus kecepatan
+    '''
+    return jarak/waktu
